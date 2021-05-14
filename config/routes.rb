51b9dcf3ceb008593d439f6aac3bb24715967720
Your Name <you@example.com>
2021-05-14 14:40:24 -0600

    done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  #scope allows us to prefix a block of routes under one grouping.
+  #namespace lets is make our route a bit less DRY and gives us the helper admin_ so now its admin_stats_path instead of stats_path.
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 
   root 'posts#index'
 end
